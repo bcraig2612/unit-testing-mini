8be69cb97a5905c18c9fbc645b16d2303fd20a46
Brandon Craig <bcraig2612@gmail.com>
2019-08-25 13:39:14 -0700

    Completed Mini Project

--- functions.js
+++ functions.js
@@ -1,11 +1,26 @@
+// Unit & Group Unit Testing 
 module.exports = {
     returnTwo: function () {
         return 2;
     },
+
     greeting: function (name) {
-        `Hello, ${name}.`;
+        return `Hello, ${name}.`;
     },
+
     add: function (num1, num2) {
         return num1 + num2;
+    },
+
+    multiply: function (num1, num2) {
+        return num1 * num2;
+    },
+
+    divide: function (num1, num2) {
+        return num1 / num2;
+    },
+
+    subtract: function (num1, num2) {
+        return num1 - num2;
     }
 };
